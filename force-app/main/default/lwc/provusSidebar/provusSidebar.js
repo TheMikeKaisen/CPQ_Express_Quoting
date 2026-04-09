b478605f9001fe0b22233f51256bd8266215b169
karthik-provus <h.karthiknair@gmail.com>
2026-04-09 10:59:09 +0530

    ADD: user role tag

--- force-app/main/default/lwc/provusSidebar/provusSidebar.js
+++ force-app/main/default/lwc/provusSidebar/provusSidebar.js
@@ -8,13 +8,36 @@ export default class ProvusSidebar extends LightningElement {
     @api activePage = 'dashboard';
 
     @track isManager = false;
+    @track userName = '';
+    @track userRole = '';
 
     @wire(getCurrentUserContext)
     wiredContext({ data }) {
         if (data) {
             this.isManager = data.isManager;
+            this.userName = data.userName;
+            
+            if (data.profileName === 'System Administrator') {
+                this.userRole = 'Admin';
+            } else if (data.isManager) {
+                this.userRole = 'Manager';
+            } else {
+                this.userRole = 'Salesperson';
+            }
         }
     }
+
+    get userInitials() {
+        if (!this.userName) return '??';
+        return this.userName.split(' ').map(n => n[0]).join('').toUpperCase().substring(0, 2);
+    }
+
+    get roleBadgeClass() {
+        if (this.userRole === 'Admin') return 'user-role-badge badge-admin';
+        if (this.userRole === 'Manager') return 'user-role-badge badge-manager';
+        return 'user-role-badge badge-sales';
+    }
+
 
     get dashboardClass() { return this.getItemClass('dashboard'); }
     get quotesClass() { return this.getItemClass('quotes'); }
